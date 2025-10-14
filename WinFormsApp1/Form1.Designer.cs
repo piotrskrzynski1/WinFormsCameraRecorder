9238106b4aaf8daf7f7a01ea4d63d50b0c748be0
Piotr Skrzyński <76049506+piotrskrzynski1@users.noreply.github.com>
2025-10-15 01:40:04 +0200

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            cboDevice = new ComboBox();
+            pictureBox = new PictureBox();
+            btnStart = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            scrnBtn = new Button();
+            button1 = new Button();
+            trackBar1 = new TrackBar();
+            trackBar2 = new TrackBar();
+            label2 = new Label();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(149, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Cameras:";
+            // 
+            // cboDevice
+            // 
+            cboDevice.FormattingEnabled = true;
+            cboDevice.Location = new Point(211, 20);
+            cboDevice.Name = "cboDevice";
+            cboDevice.Size = new Size(332, 23);
+            cboDevice.TabIndex = 2;
+            // 
+            // pictureBox
+            // 
+            pictureBox.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox.Location = new Point(12, 67);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(531, 371);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.TabIndex = 3;
+            pictureBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(564, 97);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 4;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // scrnBtn
+            // 
+            scrnBtn.Location = new Point(564, 138);
+            scrnBtn.Name = "scrnBtn";
+            scrnBtn.Size = new Size(81, 23);
+            scrnBtn.TabIndex = 5;
+            scrnBtn.Text = "Zrób Zdjęcie";
+            scrnBtn.UseVisualStyleBackColor = true;
+            scrnBtn.Click += scrnBtn_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(564, 181);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Nagraj";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(564, 337);
+            trackBar1.Maximum = 100;
+            trackBar1.Minimum = -100;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(187, 45);
+            trackBar1.TabIndex = 7;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // trackBar2
+            // 
+            trackBar2.Location = new Point(564, 266);
+            trackBar2.Maximum = 100;
+            trackBar2.Minimum = -100;
+            trackBar2.Name = "trackBar2";
+            trackBar2.Size = new Size(187, 45);
+            trackBar2.TabIndex = 8;
+            trackBar2.Scroll += trackBar2_Scroll;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(634, 296);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Jasność";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(630, 367);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Kontrast";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(trackBar2);
+            Controls.Add(trackBar1);
+            Controls.Add(button1);
+            Controls.Add(scrnBtn);
+            Controls.Add(btnStart);
+            Controls.Add(pictureBox);
+            Controls.Add(cboDevice);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Camera App";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox cboDevice;
+        private PictureBox pictureBox;
+        private Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private Button scrnBtn;
+        private Button button1;
+        private TrackBar trackBar1;
+        private TrackBar trackBar2;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice captureDevice;
+        Bitmap image;
+        private bool recording;
+        List<Bitmap> imageList;
+        private string buttontext = "";
+        private int currentContrast = 0;
+        private int currentBrightness = 0;
+
+        private void CreateMp4FromBitmaps(List<Bitmap> frames, string outputFilePath, double fps = 10.0)
+        {
+            if (frames == null || frames.Count == 0)
+            {
+                MessageBox.Show("No frames to save.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            int width = frames[0].Width;
+            int height = frames[0].Height;
+            var outDir = Path.GetDirectoryName(outputFilePath);
+            if (!Directory.Exists(outDir))
+                Directory.CreateDirectory(outDir);
+            try
+            {
+                int fourcc = VideoWriter.FourCC('M', 'P', '4', 'V');
+                using (var writer = new VideoWriter())
+                {
+                    bool opened = writer.Open(outputFilePath, fourcc, fps, new OpenCvSharp.Size(width, height));
+                    if (!opened)
+                    {
+                        MessageBox.Show("Failed to open VideoWriter. Try a different fourcc (e.g. MJPG) or check codecs.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    foreach (var bmp in frames)
+                    {
+                        if (bmp == null) continue;
+                        using (var mat = BitmapConverter.ToMat(bmp))
+                        {
+                            if (mat.Width != width || mat.Height != height)
+                            {
+                                using (var resized = mat.Resize(new OpenCvSharp.Size(width, height)))
+                                {
+                                    writer.Write(resized);
+                                }
+                            }
+                            else
+                            {
+                                writer.Write(mat);
+                            }
+                        }
+                    }
+                    writer.Release();
+                }
+                MessageBox.Show("Zapisano Wideo!", "Koniec", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Blad przy zapisie wideo: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Bitmap ApplyBrightnessContrast(Bitmap source, int brightness, int contrast)
+        {
+            using (var srcMat = BitmapConverter.ToMat(source))
+            using (var dstMat = new Mat())
+            {
+                double alpha = (contrast + 100) / 100.0; // contrast: -100..100 -> alpha 0..2
+                double beta = brightness;               // brightness: -100..100 -> beta -100..100
+                srcMat.ConvertTo(dstMat, MatType.CV_8UC3, alpha, beta);
+                return BitmapConverter.ToBitmap(dstMat);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+            {
+                cboDevice.Items.Add(filterInfo.Name);
+            }
+            if (cboDevice.Items.Count > 0)
+                cboDevice.SelectedIndex = 0;
+            imageList = new List<Bitmap>();
+            if (trackBar1 != null) currentContrast = trackBar1.Value;
+            if (trackBar2 != null) currentBrightness = trackBar2.Value;
+        }
+
+        private void CaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap frameBmp = null;
+            Bitmap adjusted = null;
+            try
+            {
+                frameBmp = (Bitmap)eventArgs.Frame.Clone();
+                adjusted = ApplyBrightnessContrast(frameBmp, currentBrightness, currentContrast);
+                if (pictureBox.Image != null) pictureBox.Image.Dispose();
+                pictureBox.Image = (Bitmap)adjusted.Clone();
+                image = (Bitmap)adjusted.Clone();
+                
+                if (recording)
+                {
+                    var clone = (Bitmap)adjusted.Clone();
+                    lock (imageList)
+                    {
+                        imageList.Add(clone);
+                    }
+                }
+            }
+            catch
+            {
+            }
+            finally
+            {
+                if (adjusted != null) adjusted.Dispose();
+                if (frameBmp != null) frameBmp.Dispose();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (captureDevice != null && captureDevice.IsRunning)
+            {
+                captureDevice.SignalToStop();
+                captureDevice.WaitForStop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            captureDevice = new VideoCaptureDevice(filterInfoCollection[cboDevice.SelectedIndex].MonikerString);
+            captureDevice.NewFrame += CaptureDevice_NewFrame;
+            captureDevice.Start();
+        }
+
+        private void scrnBtn_Click(object sender, EventArgs e)
+        {
+            if (image != null)
+            {
+                image.Dispose();
+            }
+            image = (Bitmap)pictureBox.Image;
+            if (pictureBox.Image == null)
+            {
+                MessageBox.Show("Brak obrazu do zapisu!");
+                return;
+            }
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Images (*.png)|*.png;*.bmp;*.jpg";
+            saveFileDialog.RestoreDirectory = true;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ImageFormat format = ImageFormat.Png;
+                string ext = Path.GetExtension(saveFileDialog.FileName).ToLower();
+                switch (ext)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                }
+                image.Save(saveFileDialog.FileName, format);
+                MessageBox.Show("Zapisano obraz!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+ 
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                if (buttontext == "")
+                {
+                    buttontext = btn.Text;
+            }
+                if (recording)
+                {
+                    btn.Text = buttontext;
+                } else
+                {
+                    btn.Text = "STOP";
+                }
+            }
+            if (recording)
+            {
+                recording = false;
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Videos|*.mp4";
+                    saveFileDialog.RestoreDirectory = true;
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        List<Bitmap> framesToSave;
+                        lock (imageList)
+                        {
+                            framesToSave = imageList.Select(b => (Bitmap)b.Clone()).ToList();
+                        }
+                        CreateMp4FromBitmaps(framesToSave, saveFileDialog.FileName);
+                        foreach (var bmp in framesToSave) bmp.Dispose();
+                        lock (imageList)
+                        {
+                            foreach (var bmp in imageList) bmp.Dispose();
+                            imageList.Clear();
+                        }
+                    }
+                }
+                return;
+            }
+            recording = true;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            currentContrast = trackBar1.Value;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            currentBrightness = trackBar2.Value;
+        }
+    }
+}
